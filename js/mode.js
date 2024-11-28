f3d8f2593ee1d6c6dd47b5bb7c627a27ad17df7c
go-peya <donveown@gmail.com>
2024-11-28 06:58:53 -0500

    Add files via upload

--- js/mode.js
+++ js/mode.js
@@ -16,3 +16,32 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .catch(error => console.error('Error loading mode.html:', error));
 });
+
+// Cargar el contenido del mode
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('mode.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('mode-pan').innerHTML = data;
+
+            // Inicializa el modo oscuro después de cargar el HTML
+            const themeSwitcher = document.querySelector(".theme-switcher");
+            const body = document.body;
+
+            themeSwitcher.addEventListener("click", () => {
+                // Alternar la clase "dark" en el body
+                body.classList.toggle("dark");
+
+                // Selecciona el logo
+                const logoImg = document.querySelector(".header-logo-img");
+
+                // Cambiar la imagen del logo según el modo
+                if (body.classList.contains("dark")) {
+                    logoImg.src = "img/logo_black.svg"; // Cambia al logo oscuro
+                } else {
+                    logoImg.src = "img/logo_white.svg"; // Cambia al logo claro
+                }
+            });
+        })
+        .catch(error => console.error('Error loading mode.html:', error));
+});
